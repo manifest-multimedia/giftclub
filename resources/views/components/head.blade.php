a58bad5119ff2560d5ecbca488c08cb9a704f5a4
Gabriel Abiah <gabriel@manifestghana.com>
2021-11-02 05:00:23 +0000

    home packages updated

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -13,7 +13,7 @@
 
 
     <!-- Bootstrap CSS -->
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.17/tailwind.min.css">
     <link rel="stylesheet" type="text/css" href="{{asset("css/style.css")}}">
     <link rel="stylesheet" type="text/css" href="{{asset("css/custom.scss")}}">
     <link rel="shortcut icon" type="image" href="{{asset("images/logo-icon.png")}}">
