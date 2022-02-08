398098305eb202ffcf415b2af4cb178bc4a321da
Johnson Sebire <johnson@manifestghana.com>
2022-02-08 09:45:51 +0000

    Updated Cron Task

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('check:paymentstatus')
-        ->everyMinute()->emailOutputTo('johnson@manifestghana.com');
+        ->everyMinute();
     }
 
     /**
